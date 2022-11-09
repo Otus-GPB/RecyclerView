1a68d358082324b65f9b6972ef759761c736dc03
zanoza_room <etn88@yandex.ru>
2022-11-09 12:08:36 +0300

    Add in Activity logic for working RecyclerView
    Here we create a LoaderChat object and using it for:
    1) create a listener for listen to change data of the Chat object
    2) using methods of the LoaderChat: delete or add new Chat object

--- app/src/main/java/otus/gpb/recyclerview/MainActivity.kt
+++ app/src/main/java/otus/gpb/recyclerview/MainActivity.kt
@@ -1,12 +1,58 @@
 package otus.gpb.recyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var loaderChat: LoaderChat
+    private lateinit var usersListener: UsersListener
+    private lateinit var chatList: List<Chat>
+    private lateinit var chatAdapter: ChatAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loaderChat = LoaderChat(this)
+        chatAdapter = ChatAdapter()
+        usersListener = {
+            chatList = it
+            chatAdapter.usersChat = chatList
+        }
+        loaderChat.addListener(usersListener)
+
+        val chatLayoutManager = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
+            layoutManager = chatLayoutManager
+            adapter = chatAdapter
+            addItemDecoration(MyDecorator(chatList.size))
+        }
+        val swipeCallBack = SwipeToDeleteListener(this) { position ->
+            val user = chatList[position]
+            loaderChat.deleteUser(user)
+        }
+        val itemCallBack = ItemTouchHelper(swipeCallBack)
+        itemCallBack.attachToRecyclerView(recyclerView)
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (dy > 0 && (
+                            recyclerView.layoutManager
+                                    as LinearLayoutManager
+                            ).findLastCompletelyVisibleItemPosition() == chatList.size - 1
+                ) {
+
+                    loaderChat.copy()
+                }
+            }
+        })
+    }
+
+    override fun onStop() {
+        super.onStop()
+        loaderChat.removeListener(usersListener)
     }
 }
