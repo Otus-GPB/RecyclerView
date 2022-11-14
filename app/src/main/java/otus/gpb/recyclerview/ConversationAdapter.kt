df0f9a2081e5cb7f74f92122349ffebc54baf463
DmitryM <mikhaltchenkov@gmail.com>
2022-11-14 20:47:39 +0300

    added adapter

---
+++ app/src/main/java/otus/gpb/recyclerview/ConversationAdapter.kt
@@ -0,0 +1,51 @@
+package otus.gpb.recyclerview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import otus.gpb.recyclerview.databinding.LayoutConversationItemBinding
+import otus.gpb.recyclerview.extensions.visible
+import otus.gpb.recyclerview.model.Conversation
+
+class ConversationAdapter : ListAdapter<Conversation, ConversationAdapter.ConversationViewHolder>(DiffCallback) {
+    private lateinit var layoutInflater: LayoutInflater
+
+    private object DiffCallback : DiffUtil.ItemCallback<Conversation>() {
+        override fun areItemsTheSame(oldItem: Conversation, newItem: Conversation) =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Conversation, newItem: Conversation) =
+            oldItem == newItem
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        layoutInflater = LayoutInflater.from(recyclerView.context)
+    }
+
+    class ConversationViewHolder(private val binding: LayoutConversationItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Conversation) {
+            with(binding) {
+                textViewUsername.text = item.username
+                textViewCount.text = item.newMessagesCount.toString()
+                textViewCount.visible(item.newMessagesCount > 0)
+                textViewMessage.text = item.lastMessage?.text.orEmpty()
+                textViewUserDetails.text = item.userDetails
+                textViewUserDetails.visible(item.userDetails != null)
+                imageMuted.visible(item.isMuted)
+                imageScam.visible(item.isScam)
+                imageRead.setImageResource(if (item.lastMessage?.isRead == true) R.drawable.ic_readed else R.drawable.ic_recieved)
+                imageRead.visible(item.lastMessage != null)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ConversationViewHolder =
+        ConversationViewHolder(LayoutConversationItemBinding.inflate(layoutInflater, parent, false))
+
+    override fun onBindViewHolder(holder: ConversationViewHolder, position: Int) {
+        holder.bind(getItem(holder.adapterPosition))
+    }
+}
--- app/src/main/java/otus/gpb/recyclerview/model/Message.kt
+++ app/src/main/java/otus/gpb/recyclerview/model/Message.kt
@@ -6,4 +6,5 @@ data class Message(
     val time: LocalDateTime,
     val image: String? = null,
     val text: String? = null,
+    val isRead: Boolean = false
 )
