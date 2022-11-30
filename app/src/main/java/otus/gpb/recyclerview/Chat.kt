7027008030096d44478a8fc78dafe1abe3f4d0a9
coolkosta <kostia.borzakov@gmail.com>
2022-11-30 15:58:16 +0300

    RecyclerView HomeWork

---
+++ app/src/main/java/otus/gpb/recyclerview/Chat.kt
@@ -0,0 +1,9 @@
+package otus.gpb.recyclerview
+
+data class Chat(
+    val icon: String,
+    val name: String,
+    val message: String,
+    val muted: Boolean,
+    val time: String
+)
--- app/src/main/java/otus/gpb/recyclerview/MainActivity.kt
+++ app/src/main/java/otus/gpb/recyclerview/MainActivity.kt
@@ -2,11 +2,32 @@ package otus.gpb.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recycler = findViewById<RecyclerView>(R.id.recyclerView)
+        val adapter = ChatAdapter()
+        recycler.adapter = adapter
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+
+        val swipeHandler = object : SwipeToDeleteCallback(this) {
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                adapter.removeAt(viewHolder.adapterPosition)
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeHandler)
+        itemTouchHelper.attachToRecyclerView(recycler)
+
+
     }
 }
